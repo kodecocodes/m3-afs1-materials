f8ea20a01f9f81705a1c71fc44e435d56609465a
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-04-12 04:49:52 +0300

    Update placeholder text to "Hello, SwiftUI!" across project stages

--- 04-create-an-xcode-project/Final/Bullseye/ContentView.swift
+++ 04-create-an-xcode-project/Final/Bullseye/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Xcode!")
+            Text("Hello, SwiftUI!")
         }
         .padding()
     }
