d79286a019e570bcedd6af48944b9654e1595ff1
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-05-13 17:02:09 +0300

    New project Xcode 16.0
    Signed-off-by: Franklin Byaruhanga <byaruhaf@protonmail.com>

---
+++ 07-challenge-add-view-modifiers/Starter/Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+      }
+      Button("Hit me") {
+
+      }
+    }
+  }
+}
+
+#Preview {
+    ContentView()
+}
