07e68df606a2d96ff73f9b7a26e4eda3005be5db
Pinal Naik <pinal.naik@gmail.com>
2026-05-14 13:03:50 +0530

    adjusted spacing as 2 tabs

--- 04-swiftui-views/Final/Bullseye/Bullseye/ContentView.swift
+++ 04-swiftui-views/Final/Bullseye/Bullseye/ContentView.swift
@@ -1,22 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-      Text("89")
-      HStack {
-        Text("1")
-        Slider(value: .constant(50), in: 1.0...100.0)
-        Text("100")
-      }
-      Button("Hit me") {
-
-      }
-    }
-  }
+	var body: some View {
+		VStack {
+			Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+			Text("89")
+			HStack {
+				Text("1")
+				Slider(value: .constant(50), in: 1.0...100.0)
+				Text("100")
+			}
+			Button("Hit me") {
+				
+			}
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
